c7dec61d6bf5f5bdd6c6419ffc9767abe9584b3a
npnelson <nnelson1@insight.rr.com>
2020-01-19 18:33:40 -0500

    add testharness

---
+++ test/NetToolBox.BlobStorage.TestHarness/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NetToolBox.BlobStorage.TestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        //TODO: test managed service identity authentication
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
