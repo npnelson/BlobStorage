6e238928ee599d93fdcf9418352d5e3844e1b32c
Nicholas P Nelson <nnelson1@insight.rr.com>
2020-01-22 11:58:42 -0500

    Feature/initial (#1)

---
+++ src/NetToolBox.BlobStorage.Azure/AzureBlobStorageFactory.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Identity;
+using Azure.Storage.Blobs;
+using NETToolBox.BlobStorage.Abstractions;
+using System;
+using System.Collections.Concurrent;
+
+namespace NetToolBox.BlobStorage.Azure
+{
+    public sealed class AzureBlobStorageFactory : IBlobStorageFactory
+    {       
+        private readonly ConcurrentDictionary<(string accountName, string containerName), BlobContainerClient> _blobContainerClientDictionary = new ConcurrentDictionary<(string accountName, string containerName), BlobContainerClient>();
+        private readonly ConcurrentDictionary<(string accountName, string containerName), IBlobStorage> _blobStorageDictionary = new ConcurrentDictionary<(string accountName, string containerName), IBlobStorage>();
+        public IBlobStorage GetBlobStorage(string accountName, string containerName)
+        {
+            IBlobStorage retval;
+           
+
+            if (!_blobContainerClientDictionary.ContainsKey((accountName, containerName)))
+            {
+                string containerEndpoint = string.Format("https://{0}.blob.core.windows.net/{1}",
+                                                         accountName,
+                                                         containerName);
+                BlobContainerClient containerClient = new BlobContainerClient(new Uri(containerEndpoint),
+                                                                           new DefaultAzureCredential(true)); //call an interactive login until https://github.com/Azure/azure-sdk-for-net/issues/8658 is fixed
+                containerClient.CreateIfNotExists();
+                _blobContainerClientDictionary.TryAdd((accountName, containerName), containerClient);
+                _blobStorageDictionary.TryAdd((accountName, containerName), new AzureBlobStorage(_blobContainerClientDictionary[(accountName, containerName)]));
+
+                //create container if it doesn't exist
+               
+            }
+            retval = _blobStorageDictionary[(accountName, containerName)];
+            return retval;
+        }
+    }
+}
---
+++ test/NetToolBox.BlobStorage.TestHarness/Program.cs
@@ -0,0 +1,22 @@
+﻿using NetToolBox.BlobStorage.Azure;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetToolBox.BlobStorage.TestHarness
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {          
+            var blobFactory = new AzureBlobStorageFactory();
+            var blobStorage = blobFactory.GetBlobStorage("mohpreferenceblobstorage", "testcontainer");
+            string blobContents = DateTime.UtcNow.ToString();
+            byte[] byteArray = Encoding.ASCII.GetBytes(blobContents);
+            await blobStorage.StoreBlobAsBytesAsync("testblob.txt",byteArray);
+            Console.WriteLine($"Wrote Blob {blobContents}");
+        }
+
+     
+    }
+}
