d2694d3006c238f5dbfd780316ca1fbb0d9f2a44
npnelson <nnelson1@insight.rr.com>
2020-04-25 09:08:01 -0400

    WIP

--- src/NetToolBox.BlobStorage.Azure/AzureBlobStorage.cs
+++ src/NetToolBox.BlobStorage.Azure/AzureBlobStorage.cs
@@ -28,34 +28,34 @@ namespace NetToolBox.BlobStorage.Azure
         public async Task<string> DownloadFileAsTextAsync(string blobPath, CancellationToken cancellationToken = default)
         {
             string retval;
-            var download = await DownloadFileAsStreamAsync(blobPath,cancellationToken).ConfigureAwait(false);
+            var download = await DownloadFileAsStreamAsync(blobPath, cancellationToken).ConfigureAwait(false);
             using (var sr = new StreamReader(download))
             {
                 retval = await sr.ReadToEndAsync().ConfigureAwait(false);
             }
             return retval;
         }
-      
+
         public async Task StoreBlobAsBytesAsync(string blobPath, byte[] byteArray, CancellationToken cancellationToken = default)
         {
             Stream ms = new MemoryStream(byteArray);
             await StoreBlobAsStreamAsync(blobPath, ms, cancellationToken).ConfigureAwait(false);
         }
 
-      
+
 
         public async Task StoreBlobAsStreamAsync(string blobPath, Stream stream, CancellationToken cancellationToken = default)
         {
-            //TODO: documentation indicates uploadblob should overwrite, but it appears not to
-            //workaround by deleteing for now
-            await _blobContainerClient.DeleteBlobIfExistsAsync(blobName:blobPath,cancellationToken:cancellationToken).ConfigureAwait(false);
+            //documentation indicates uploadblob should overwrite, but it appears not to
+            //workaround by deleting for now
+            await _blobContainerClient.DeleteBlobIfExistsAsync(blobName: blobPath, cancellationToken: cancellationToken).ConfigureAwait(false);
             await _blobContainerClient.UploadBlobAsync(blobPath, stream, cancellationToken).ConfigureAwait(false);
         }
-     
+
         public async Task StoreBlobAsTextAsync(string blobPath, string blobContents, CancellationToken cancellationToken = default)
         {
             byte[] byteArray = Encoding.ASCII.GetBytes(blobContents);
-            await StoreBlobAsBytesAsync(blobPath, byteArray, cancellationToken).ConfigureAwait(false) ;
+            await StoreBlobAsBytesAsync(blobPath, byteArray, cancellationToken).ConfigureAwait(false);
         }
     }
 }
