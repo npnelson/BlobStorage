49cccca4c351e45aa328fcfc50e577d19a7e90b3
npnelson <nnelson1@insight.rr.com>
2020-05-15 16:04:31 -0400

    added ability to load container references at construction to make health checks better at startup

--- test/NetToolBox.BlobStorage.TestHarness/Program.cs
+++ test/NetToolBox.BlobStorage.TestHarness/Program.cs
@@ -9,8 +9,15 @@ namespace NetToolBox.BlobStorage.TestHarness
     {
         static async Task Main(string[] args)
         {
+            //var blobFactory = new AzureBlobStorageFactory(new System.Collections.Generic.List<(string accountName, string containerName)> { ("testStorage", "testContainer") });
             var blobFactory = new AzureBlobStorageFactory();
-            var blobStorage = blobFactory.GetBlobStorage("mohpreferenceblobstorage", "testcontainer");
+
+            var blobStorage = blobFactory.GetBlobStorage("validaccount", "testcontainer", false);
+            var registrations = blobFactory.GetBlobStorageRegistrations();
+            foreach (var container in registrations)
+            {
+                var retval = await blobFactory.GetBlobStorage(container.accountName, container.containerName).IsHealthyAsync();
+            }
             string blobContents = DateTime.UtcNow.ToString();
             byte[] byteArray = Encoding.ASCII.GetBytes(blobContents);
             await blobStorage.StoreBlobAsBytesAsync("testblob.txt", byteArray);
